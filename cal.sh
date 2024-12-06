83a594de993b2ac04f7a185a0697f0121709c8ec
mssai <seshasai1919@gmail.com>
2024-12-06 23:07:36 +0530

    Deploying a caliculator applicatio with add and sub

---
+++ cal.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+##############
+#Author: M S Sai
+#This is a basic caliculator application
+#############
+# Method of Addition two intigers:
+A+B = C
+
+# MEthod of Substraction two intigers:
+A-B = D
